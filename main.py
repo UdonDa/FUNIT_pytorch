d3cf029b9281e9043674aeaa2efb472a8aced79e
UdonDa <udoooon0727@gmail.com>
2019-07-10 20:55:24 +0900

    fix

--- main.py
+++ main.py
@@ -4,35 +4,43 @@ from solver import Solver
 from data_loader import get_loader
 from torch.backends import cudnn
 
+from sys import exit
 
-def main(config):
+
+def main(aegs):
     # For fast training.
     cudnn.benchmark = True
 
     # Create directories if not exist.
-    os.makedirs(config.log_dir, exist_ok=True)
-    os.makedirs(config.model_save_dir, exist_ok=True)
-    os.makedirs(config.sample_dir, exist_ok=True)
-    os.makedirs(config.result_dir, exist_ok=True)
+    os.makedirs(args.results_dir    , exist_ok=True)
+    os.makedirs(args.log_dir, exist_ok=True)
+    os.makedirs(args.model_save_dir, exist_ok=True)
+    os.makedirs(args.sample_dir, exist_ok=True)
+    os.makedirs(args.result_dir, exist_ok=True)
 
-    loader = get_loader(config.rafd_image_dir, None, None,
-                            config.rafd_crop_size, config.image_size, config.batch_size,
-                            'RaFD', config.mode, config.num_workers)
+    loader = get_loader(args.image_dir,
+                        args.crop_size,
+                        args.image_size,
+                        args.batch_size,
+                        args.mode,
+                        args.num_workers)
 
-    solver = Solver(loader, config)
+    solver = Solver(loader, args)
 
-    if config.mode == 'train':
+    if args.mode == 'train':
         solver.train()
-    elif config.mode == 'test':
+    elif args.mode == 'test':
         solver.test()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    # Model configuration.
-    parser.add_argument('--crop_size', type=int, default=256, help='crop size for the RaFD dataset')
-    parser.add_argument('--image_size', type=int, default=286, help='image resolution')
+    name = "debug"
+
+    # Model argsuration.
+    parser.add_argument('--crop_size', type=int, default=286, help='crop size for the RaFD dataset')
+    parser.add_argument('--image_size', type=int, default=256, help='image resolution')
     parser.add_argument('--g_conv_dim', type=int, default=64, help='number of conv filters in the first layer of G')
     parser.add_argument('--d_conv_dim', type=int, default=64, help='number of conv filters in the first layer of D')
     parser.add_argument('--g_repeat_num', type=int, default=6, help='number of residual blocks in G')
@@ -41,8 +49,9 @@ if __name__ == '__main__':
     parser.add_argument('--lambda_rec', type=float, default=10, help='weight for reconstruction loss')
     parser.add_argument('--lambda_gp', type=float, default=10, help='weight for gradient penalty')
     
-    # Training configuration.
+    # Training argsuration.
     parser.add_argument('--dataset', type=str, default='food')
+    parser.add_argument('--epochs', type=int, default=200, help='mini-batch size')
     parser.add_argument('--batch_size', type=int, default=16, help='mini-batch size')
     parser.add_argument('--g_lr', type=float, default=0.0001, help='learning rate for G')
     parser.add_argument('--d_lr', type=float, default=0.0001, help='learning rate for D')
@@ -50,7 +59,7 @@ if __name__ == '__main__':
     parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for Adam optimizer')
     parser.add_argument('--beta2', type=float, default=0.999, help='beta2 for Adam optimizer')
 
-    # Test configuration.
+    # Test argsuration.
     parser.add_argument('--test_iters', type=int, default=200000, help='test model from this step')
 
     # Miscellaneous.
@@ -59,17 +68,18 @@ if __name__ == '__main__':
 
     # Directories.
     parser.add_argument('--image_dir', type=str, default='data/food')
-    parser.add_argument('--log_dir', type=str, default='stargan/logs')
-    parser.add_argument('--model_save_dir', type=str, default='stargan/models')
-    parser.add_argument('--sample_dir', type=str, default='stargan/samples')
-    parser.add_argument('--result_dir', type=str, default='stargan/results')
+
+    parser.add_argument('--results_dir', type=str, default='results/')
+    parser.add_argument('--log_dir', type=str, default=f'results/{name}/logs')
+    parser.add_argument('--model_save_dir', type=str, default=f'results/{name}/models')
+    parser.add_argument('--sample_dir', type=str, default=f'results/{name}/samples')
+    parser.add_argument('--result_dir', type=str, default=f'results/{name}/results')
 
     # Step size.
     parser.add_argument('--log_step', type=int, default=10)
     parser.add_argument('--sample_step', type=int, default=1000)
     parser.add_argument('--model_save_step', type=int, default=10000)
-    parser.add_argument('--lr_update_step', type=int, default=1000)
 
-    config = parser.parse_args()
-    print(config)
-    main(config)
+    args = parser.parse_args()
+    print(args)
+    main(args)
--- model.py
+++ model.py
@@ -21,11 +21,11 @@ class ResidualBlock(nn.Module):
 
 class Generator(nn.Module):
     """Generator network."""
-    def __init__(self, conv_dim=64, c_dim=5, repeat_num=6):
+    def __init__(self, conv_dim=64, repeat_num=6):
         super(Generator, self).__init__()
 
         layers = []
-        layers.append(nn.Conv2d(3+c_dim, conv_dim, kernel_size=7, stride=1, padding=3, bias=False))
+        layers.append(nn.Conv2d(3, conv_dim, kernel_size=7, stride=1, padding=3, bias=False))
         layers.append(nn.InstanceNorm2d(conv_dim, affine=True, track_running_stats=True))
         layers.append(nn.ReLU(inplace=True))
 
@@ -64,7 +64,7 @@ class Generator(nn.Module):
 
 class Discriminator(nn.Module):
     """Discriminator network with PatchGAN."""
-    def __init__(self, image_size=128, conv_dim=64, c_dim=5, repeat_num=6):
+    def __init__(self, image_size=128, conv_dim=64, repeat_num=6):
         super(Discriminator, self).__init__()
         layers = []
         layers.append(nn.Conv2d(3, conv_dim, kernel_size=4, stride=2, padding=1))
@@ -79,10 +79,8 @@ class Discriminator(nn.Module):
         kernel_size = int(image_size / np.power(2, repeat_num))
         self.main = nn.Sequential(*layers)
         self.conv1 = nn.Conv2d(curr_dim, 1, kernel_size=3, stride=1, padding=1, bias=False)
-        self.conv2 = nn.Conv2d(curr_dim, c_dim, kernel_size=kernel_size, bias=False)
         
     def forward(self, x):
         h = self.main(x)
         out_src = self.conv1(h)
-        out_cls = self.conv2(h)
-        return out_src, out_cls.view(out_cls.size(0), out_cls.size(1))
+        return out_src
